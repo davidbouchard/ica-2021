64a3fe35b65a2d10fce3aabbc748097eb43aae6e
davidbouchard <david.bouchard@ryerson.ca>
2021-05-11 11:07:21 -0400

    missed one
    missed one example in week1

---
+++ week-1/p5-first-steps/sketch.js
@@ -0,0 +1,29 @@
+// ------------------------------------------------------------------
+// Code placed in setup() will run once at the beginning
+function setup() {
+  // Create a new canvas to match the browser size
+  createCanvas(windowWidth, windowHeight);
+  background("gray");
+}
+
+// ------------------------------------------------------------------
+// On window resize, update the canvas size
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+// ------------------------------------------------------------------
+// Main render loop - code placed in draw() will repeat over and over
+function draw() {
+  // Place your drawing code here
+
+  if (mouseIsPressed == true) {
+    fill("black");
+    stroke("white");
+  } else {
+    fill("white");
+    stroke("black");
+  }
+
+  circle(mouseX, mouseY, 100);
+}
